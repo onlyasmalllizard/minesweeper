7cc9589e4b9a6d8b778a8889588546b2f11447b7
onlyasmalllizard <lizardmorrow@gmail.com>
2020-05-12 12:55:56 +0100

    Add colour to display

--- tile.rb
+++ tile.rb
@@ -1,3 +1,5 @@
+require 'colorize'
+
 class Tile
 
     attr_accessor :value
@@ -6,8 +8,9 @@ class Tile
 
     def initialize(is_bomb, value)
         @bomb = is_bomb
-        @value = value
+        @value = colour_value(value)
         @faceup = false
+        @tile_back = " ".colorize(:background => :light_black)
     end
 
     def reveal
@@ -16,6 +19,16 @@ class Tile
 
     def is_bomb?
         @bomb
+    end
+
+    def colour_value(value)
+        colours = { 1 => :blue, 2 => :light_magenta, 3 => :magenta, 4 => :green, 5 => :cyan,
+                    6 => :yellow, 7 => :yellow, 8 => :light_red }
+
+        return value.colorize(:red).on_white if self.is_bomb?
+        return " ".colorize(:background => :white) if value == 0
+
+        value.to_s.colorize(colours[value]).on_white
     end
 
 end
